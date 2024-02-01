b5011257d18073962e63dd2069ba2dc120fb06a9
Humboorgir <hardcraftap@gmail.com>
2024-02-01 14:09:51 +0330

    feat(tailwind): configure colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      color: {
+        foreground: {
+          DEFAULT: "hsl(var(--foreground))",
+          light: "hsl(var(--foreground-light))",
+          muted: "hsl(var(--foreground-muted))",
+        },
+        background: "hsl(var(--background))",
+        primary: "hsl(var(--primary))",
+        secondary: "hsl(var(--secondary))",
+        accent: "hsl(var(--accent))",
       },
     },
   },
