7e2a38d68fd53e10cf0212087abcd3781de6978c
Humboorgir <hardcraftap@gmail.com>
2024-02-21 15:58:29 +0330

    fix(contact): reveal animation delayed when scrolling

--- src/components/home/contact-section/contact-section.tsx
+++ src/components/home/contact-section/contact-section.tsx
@@ -7,15 +7,19 @@ import Button from "@/components/ui/button";
 import Row from "@/components/ui/row";
 import Tooltip from "@/components/ui/tooltip";
 
-import { cubicBezier, m } from "framer-motion";
+import { cubicBezier, m, useInView } from "framer-motion";
 
 import { AiOutlineMail as Mail } from "react-icons/ai";
 import { IoLogoDiscord as Discord } from "react-icons/io5";
 import { FaTelegram as Telegram } from "react-icons/fa6";
 import { FaTwitter as Twitter } from "react-icons/fa6";
 import { RiQuestionAnswerLine as Project } from "react-icons/ri";
+import { useRef } from "react";
 
 const ContactSection = () => {
+  const titleRef = useRef(null);
+  const visible = useInView(titleRef);
+
   const MText = m(Text);
   const MInput = m(Input);
   const MTextArea = m(TextArea);
@@ -30,7 +34,7 @@ const ContactSection = () => {
   return (
     <Container className="flex flex-col items-center">
       <Column items="center" className="text-center p-10 z-10 mb-4">
-        <Row className="text-blue-500 !text-lg !flex !items-center space-x-1">
+        <Row ref={titleRef} className="text-blue-500 !text-lg !flex !items-center space-x-1">
           <Project className="text-lg mr-1.5 mb-0.5" />
           {"So what are you waiting for?".split(" ").map((text, i) => {
             return (
@@ -38,56 +42,59 @@ const ContactSection = () => {
                 initial="hidden"
                 whileInView="shown"
                 variants={mVariants}
-                transition={{ delay: (i + 1) / 14, ease: mEase, duration: 0.5 }}>
+                transition={{ delay: (i + 1) / 10, ease: mEase, duration: 0.5 }}>
                 {text}
               </m.span>
             );
           })}
         </Row>
-
-        <MText
-          initial="hidden"
-          whileInView="shown"
-          variants={mVariants}
-          transition={{ delay: 0.9, ease: mEase, duration: 1 }}
-          className="text-foreground/90 z-20 !text-4xl md:!text-6xl"
-          variant="h2">
-          Let's build the future!
-        </MText>
-        <MText
-          initial="hidden"
-          whileInView="shown"
-          variants={mVariants}
-          transition={{ delay: 1.18, ease: mEase, duration: 0.7 }}
-          className="max-w-xl mb-5"
-          variant="p">
-          Send me a message, make sure to include necessary details like your project and how I can
-          contact you back
-        </MText>
-        <MInput
-          initial="hidden"
-          whileInView="shown"
-          variants={mVariants}
-          transition={{ delay: 1.24, ease: mEase, duration: 0.7 }}
-          className="w-full mb-2.5 self-start"
-          placeholder="Name"
-        />
-        <MTextArea
-          initial="hidden"
-          whileInView="shown"
-          variants={mVariants}
-          transition={{ delay: 1.35, ease: mEase, duration: 0.7 }}
-          className="w-full mb-3"
-          placeholder="Message"
-        />
-        <MButton
-          className="w-full bg-gradient-to-br from-blue-600 to-blue-800"
-          initial="hidden"
-          whileInView="shown"
-          variants={mVariants}
-          transition={{ delay: 1.4, ease: mEase, duration: 0.7 }}>
-          Send project request <Mail className="ml-1" />
-        </MButton>
+        {visible && (
+          <>
+            <MText
+              initial="hidden"
+              animate="shown"
+              variants={mVariants}
+              transition={{ delay: 1.2, ease: mEase, duration: 1 }}
+              className="text-foreground/90 z-20 !text-4xl md:!text-6xl"
+              variant="h2">
+              Let's build the future!
+            </MText>
+            <MText
+              initial="hidden"
+              animate="shown"
+              variants={mVariants}
+              transition={{ delay: 1.3, ease: mEase, duration: 0.7 }}
+              className="max-w-xl mb-5"
+              variant="p">
+              Send me a message, make sure to include necessary details like your project and how I can
+              contact you back
+            </MText>
+            <MInput
+              initial="hidden"
+              animate="shown"
+              variants={mVariants}
+              transition={{ delay: 1.4, ease: mEase, duration: 0.7 }}
+              className="w-full mb-2.5 self-start"
+              placeholder="Name"
+            />
+            <MTextArea
+              initial="hidden"
+              animate="shown"
+              variants={mVariants}
+              transition={{ delay: 1.5, ease: mEase, duration: 0.7 }}
+              className="w-full mb-3"
+              placeholder="Message"
+            />
+            <MButton
+              className="w-full bg-gradient-to-br from-blue-600 to-blue-800"
+              initial="hidden"
+              animate="shown"
+              variants={mVariants}
+              transition={{ delay: 1.6, ease: mEase, duration: 0.7 }}>
+              Send project request <Mail className="ml-1" />
+            </MButton>
+          </>
+        )}
       </Column>
 
       <Text variant="h4" className="text-foreground/80">
