1e06f09564a07cadf73356c60d57069f0f49b3fc
Humboorgir <hardcraftap@gmail.com>
2024-02-22 20:12:35 +0330

    fix(home): make page responsive

--- src/components/home/experience-section/experience-section.tsx
+++ src/components/home/experience-section/experience-section.tsx
@@ -10,6 +10,31 @@ import { GiAlarmClock as Clock } from "react-icons/gi";
 import Image from "next/image";
 
 const ExperienceSection = () => {
+  const items = [
+    {
+      name: "Front-end Development",
+      description:
+        "Experienced with most major javascript frameworks, such as React, Svelte, Vue and meta frameworks like Next",
+      image: "/laptop.svg",
+    },
+    {
+      name: "Back-end Development",
+      description: "Experienced with frameworks like Express and building Real-time apps with SocketIO",
+      image: "/process.svg",
+    },
+    {
+      name: "Always On Time",
+      description:
+        "Being Punctual is a very important value to me therefore I always try to keep things on time",
+      image: "/clock.svg",
+    },
+    {
+      name: "Experienced with it",
+      description:
+        "I've been doing web development since early 2020 and consider myself to be quite good at my job",
+      image: "/expert.svg",
+    },
+  ];
   return (
     <>
       <Container>
@@ -25,66 +50,31 @@ const ExperienceSection = () => {
       </Container>
 
       <Container className="mb-44">
-        <Row className="flex-wrap">
-          <Row
-            items="start"
-            className="h-[200px] border border-ring rounded-md flex max-w-xl p-4 mb-4 mr-4">
-            <Column items="start">
-              <div className="rounded-full p-1 bg-white/80 mb-2">
-                <React className="text-2xl text-black" />
-              </div>
-              <Text variant="h4">Front-end Development</Text>
-              <Text variant="p">
-                Experienced with most major javascript frameworks, such as React, Svelte, Vue and meta
-                frameworks like Next
-              </Text>
-            </Column>
-            <Image className="h-full mt-2" width={230} height={120} src="/laptop.svg" alt="laptop" />
-          </Row>
-          <Row
-            items="start"
-            className="min-h-[1px] h-[200px] border border-ring rounded-md flex max-w-xl mb-4 p-4">
-            <Column items="start">
-              <div className="rounded-full p-1 bg-white/80 mb-2">
-                <Process className="text-2xl text-black" />
-              </div>
-              <Text variant="h4">Back-end Development</Text>
-              <Text variant="p">
-                Experienced with frameworks like Express and building Real-time apps with SocketIO
-              </Text>
-            </Column>
-            <Image className="h-full mt-2" width={230} height={120} src="/process.svg" alt="laptop" />
-          </Row>
-          <Row
-            items="start"
-            className="min-h-[1px] h-[200px] border border-ring rounded-md flex max-w-xl p-4 mr-4">
-            <Column items="start">
-              <div className="rounded-full p-1 bg-white/80 mb-2">
-                <Clock className="text-2xl text-black" />
-              </div>
-              <Text variant="h4">Always On Time</Text>
-              <Text variant="p">
-                Being Punctual is a very important value to me therefore I always try to keep things on
-                time
-              </Text>
-            </Column>
-            <Image className="h-full mt-2" width={230} height={120} src="/clock.svg" alt="laptop" />
-          </Row>
-          <Row
-            items="start"
-            className="min-h-[1px] h-[200px] border border-ring rounded-md flex max-w-xl p-4">
-            <Column items="start">
-              <div className="rounded-full p-1 bg-white/80 mb-2">
-                <Process className="text-2xl text-black" />
-              </div>
-              <Text variant="h4">Experienced with it</Text>
-              <Text variant="p">
-                I've been doing web development since early 2020 and consider myself to be quite good at
-                my job
-              </Text>
-            </Column>
-            <Image className="h-full mt-2" width={230} height={120} src="/expert.svg" alt="laptop" />
-          </Row>
+        <Row className="flex-wrap justify-center">
+          {items.map((item) => {
+            return (
+              <Column
+                items="start"
+                justify="between"
+                className="min-h-[200px] md:h-[1px] min-w-[1px] border border-ring rounded-md max-w-xl p-4 mb-4 mr-4
+              md:flex-row">
+                <Column className="grow" items="start">
+                  <div className="rounded-full p-1 bg-white/80 mb-2">
+                    <React className="text-2xl text-black" />
+                  </div>
+                  <Text variant="h4">{item.name}</Text>
+                  <Text variant="p">{item.description}</Text>
+                </Column>
+                <Image
+                  className="self-center md:self-start h-full mt-6 md:mt-2"
+                  width={230}
+                  height={120}
+                  src={item.image}
+                  alt={item.name}
+                />
+              </Column>
+            );
+          })}
         </Row>
       </Container>
     </>
