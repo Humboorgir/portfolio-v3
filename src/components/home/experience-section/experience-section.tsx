98664a6ab1716cb35da5c1ef9c63043aa7230d1a
Humboorgir <hardcraftap@gmail.com>
2024-02-25 13:46:09 +0330

    feat(experienceSection): implement reveal animations

--- src/components/home/experience-section/experience-section.tsx
+++ src/components/home/experience-section/experience-section.tsx
@@ -9,62 +9,87 @@ import { GiAlarmClock as Clock } from "react-icons/gi";
 import { PiStackLight as Stack } from "react-icons/pi";
 
 import Image from "next/image";
+import { cubicBezier, m } from "framer-motion";
 
 const ExperienceSection = () => {
-  const items = [
-    {
-      name: "Front-end Development",
-      description:
-        "Experienced with most major javascript frameworks, such as React, Svelte, Vue and meta frameworks like Next",
-      image: "/laptop.svg",
-    },
-    {
-      name: "Back-end Development",
-      description: "Experienced with frameworks like Express and building Real-time apps with SocketIO",
-      image: "/process.svg",
-    },
-    {
-      name: "Always On Time",
-      description:
-        "Being Punctual is a very important value to me therefore I always try to keep things on time",
-      image: "/clock.svg",
-    },
-    {
-      name: "Experienced with it",
-      description:
-        "I've been doing web development since early 2020 and consider myself to be quite good at my job",
-      image: "/expert.svg",
-    },
-  ];
+  const MText = m(Text);
+  const MImage = m(Image);
+
   return (
     <>
-      <Container className="mb-44 relative w-full border-l border-l-ring sm:px-0 px-0">
+      <Container className="mb-44 relative w-full sm:px-0 px-0">
+        {/* border left */}
+        <m.div
+          initial={{ scaleY: 0 }}
+          whileInView={{ scaleY: 1 }}
+          transition={{ duration: 4, ease: cubicBezier(0.22, 1, 0.36, 1) }}
+          viewport={{ once: true }}
+          className="absolute left-0 top-0 h-full w-[1px] bg-foreground/50 origin-top z-10"
+        />
+
+        {/* title  */}
         <div className="pl-6">
-          <Text className="flex items-center mb-3 text-foreground/50 lg:text-[17px]" variant="p">
+          <MText
+            initial={{ x: 15, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{ duration: 1, ease: cubicBezier(0.22, 1, 0.36, 1) }}
+            viewport={{ once: true }}
+            className="flex items-center mb-3 text-foreground/50 lg:text-[17px]"
+            variant="p">
             <Stack className="mr-1 text-lg" /> Programming since 2020
-          </Text>
-          <Text className="mb-10 text-foreground/90 lg:text-3xl max-w-sm font-medium" variant="h3">
+          </MText>
+          <MText
+            initial={{ x: 15, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{ duration: 1, delay: 0.15, ease: cubicBezier(0.22, 1, 0.36, 1) }}
+            viewport={{ once: true }}
+            className="mb-10 text-foreground/90 lg:text-3xl max-w-sm font-medium"
+            variant="h3">
             4 Years of experience, including both Backend and Frontend development
-          </Text>
+          </MText>
         </div>
 
-        <Row className="grid place-items-center grid-cols-2">
+        <Row className="relative grid place-items-center grid-cols-2">
+          {/* border  */}
+          <m.div
+            initial={{ scaleX: 0 }}
+            whileInView={{ scaleX: 1 }}
+            transition={{ duration: 2, ease: cubicBezier(0.22, 1, 0.36, 1) }}
+            viewport={{ once: true }}
+            className="absolute left-0 top-0 w-full h-[1px] bg-white/50 origin-left"
+          />
           <Column
             items="start"
             justify="between"
-            className="relative min-h-[200px] md:h-[1px] min-w-[1px] border-t border-t-ring p-4
+            className="relative min-h-[200px] md:h-[1px] min-w-[1px] p-4
               md:flex-row bg-black/40 z-10">
             <div className="absolute left-5 -top-4 rounded-full p-1.5 border border-foreground/50 bg-background">
               <Frontend className="h-[26px] w-[26px]" />
             </div>
             <Column className="grow pt-4" items="start">
-              <Text variant="h4">Front-end development</Text>
-              <Text variant="p">
+              <MText
+                initial={{ x: 15, opacity: 0 }}
+                whileInView={{ x: 0, opacity: 1 }}
+                transition={{ duration: 1, delay: 0.2, ease: cubicBezier(0.22, 1, 0.36, 1) }}
+                viewport={{ once: true }}
+                variant="h4">
+                Front-end development
+              </MText>
+              <MText
+                initial={{ x: 15, opacity: 0 }}
+                whileInView={{ x: 0, opacity: 1 }}
+                transition={{ duration: 1, delay: 0.25, ease: cubicBezier(0.22, 1, 0.36, 1) }}
+                viewport={{ once: true }}
+                variant="p">
                 Experienced with most major javascript frameworks, such as React, Svelte, Vue and meta
                 frameworks like Next
-              </Text>
+              </MText>
             </Column>
-            <Image
+            <MImage
+              initial={{ y: 15, opacity: 0 }}
+              whileInView={{ y: 0, opacity: 1 }}
+              transition={{ duration: 1, delay: 0.3, ease: cubicBezier(0.22, 1, 0.36, 1) }}
+              viewport={{ once: true }}
               className="self-center md:self-start h-full mt-6 md:mt-2"
               width={230}
               height={120}
@@ -75,21 +100,45 @@ const ExperienceSection = () => {
           <Column
             items="start"
             justify="between"
-            className="relative min-h-[200px] md:h-[1px] min-w-[1px] border-t border-l border-ring p-4
+            className="relative min-h-[200px] md:h-[1px] min-w-[1px] p-4
               md:flex-row bg-black/40 z-10">
+            {/* border  */}
+            <m.div
+              initial={{ scaleY: 0 }}
+              whileInView={{ scaleY: 1 }}
+              transition={{ duration: 2, delay: 0.4, ease: cubicBezier(0.22, 1, 0.36, 1) }}
+              viewport={{ once: true }}
+              className="absolute left-0 top-0 h-full w-[1px] bg-white/50 origin-top"
+            />
             <div
               className="absolute left-5 -top-4 rounded-full p-2 border border-foreground/50 bg-background
             ">
               <Code className="h-6 w-6" />
             </div>
             <Column className="grow pt-4" items="start">
-              <Text variant="h4">Back-end development</Text>
-              <Text variant="p">
+              <MText
+                initial={{ x: 15, opacity: 0 }}
+                whileInView={{ x: 0, opacity: 1 }}
+                transition={{ duration: 1, delay: 0.35, ease: cubicBezier(0.22, 1, 0.36, 1) }}
+                viewport={{ once: true }}
+                variant="h4">
+                Back-end development
+              </MText>
+              <MText
+                initial={{ x: 15, opacity: 0 }}
+                whileInView={{ x: 0, opacity: 1 }}
+                transition={{ duration: 1, delay: 0.4, ease: cubicBezier(0.22, 1, 0.36, 1) }}
+                viewport={{ once: true }}
+                variant="p">
                 Experienced with frameworks like Express and Nest and building Real-time apps with
                 SocketIO
-              </Text>
+              </MText>
             </Column>
-            <Image
+            <MImage
+              initial={{ y: 15, opacity: 0 }}
+              whileInView={{ y: 0, opacity: 1 }}
+              transition={{ duration: 1, delay: 0.45, ease: cubicBezier(0.22, 1, 0.36, 1) }}
+              viewport={{ once: true }}
               className="self-center md:self-start h-full mt-6 md:mt-2"
               width={230}
               height={120}
@@ -110,18 +159,33 @@ const ExperienceSection = () => {
               <Code className="h-6 w-6" />
             </div>
             <Column className="grow pt-4" items="start">
-              <Text className="lg:text-3xl font-medium" variant="h3">
+              <MText
+                initial={{ x: 15, opacity: 0 }}
+                whileInView={{ x: 0, opacity: 1 }}
+                transition={{ duration: 1, delay: 0.45, ease: cubicBezier(0.22, 1, 0.36, 1) }}
+                viewport={{ once: true }}
+                variant="h4">
                 High quality code
-              </Text>
-              <Text className="max-w-2xl" variant="p">
+              </MText>
+              <MText
+                initial={{ x: 15, opacity: 0 }}
+                whileInView={{ x: 0, opacity: 1 }}
+                transition={{ duration: 1, delay: 0.4, ease: cubicBezier(0.22, 1, 0.36, 1) }}
+                viewport={{ once: true }}
+                className="max-w-2xl"
+                variant="p">
                 Familiar with the proper ways of writing code. The code returned will be clean and
                 readable; Decent speed and bundle size will all be considered along the way.
-              </Text>
+              </MText>
             </Column>
-            <Image
+            <MImage
+              initial={{ y: 15, opacity: 0 }}
+              whileInView={{ y: 0, opacity: 1 }}
+              transition={{ duration: 1, delay: 0.35, ease: cubicBezier(0.22, 1, 0.36, 1) }}
+              viewport={{ once: true }}
               className="self-center md:self-start mt-6 md:mt-2"
-              width={300}
-              height={220}
+              width={240}
+              height={180}
               src="/code.svg"
               alt="High quality code"
             />
