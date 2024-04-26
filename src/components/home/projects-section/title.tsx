c7be48ebaa7db4c3db8b6d48647eba27d499c032
Humboorgir <hardcraftap@gmail.com>
2024-04-26 18:47:29 +0330

    feat: rework projects section title

--- src/components/home/projects-section/title.tsx
+++ src/components/home/projects-section/title.tsx
@@ -1,27 +1,25 @@
 import Column from "@/components/ui/column";
 import Text from "@/components/ui/text";
 import Image from "next/image";
+import { IoSearch } from "react-icons/io5";
+import { PiDetectiveFill } from "react-icons/pi";
 
 const Title = () => {
   return (
-    <Column
-      items="start"
-      className="relative p-6 md:p-14 md:!pt-44 max-w-[50vw] mr-auto
-    border-left-2 border-top-2 border-ring">
-      <div className="bg-white/10 absolute left-0 right-0 top-0 -bottom-full rounded-full blur-3xl z-10" />
-      <div
-        className="absolute top-0 left-0 right-0 -bottom-full z-10 inset-0 pointer-events-none flex items-center justify-center
- dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
-      />
-      <div className="absolute left-0 right-0 top-0 -bottom-full bg-grid-[#ffffff25]" />
-
+    <Column className="relative mb-16 p-5 px-10">
       <Text
-        className="relative bg-clip-text text-left text-transparent bg-gradient-to-b from-neutral-100 to-neutral-400"
+        className="flex items-center relative bg-clip-text text-xl text-transparent text-center bg-gradient-to-b from-neutral-100 to-neutral-400"
+        variant="lead">
+        <PiDetectiveFill className="h-7 w-7 text-neutral-300 mr-2" /> Take a look at
+      </Text>
+      <Text
+        className="relative bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-100 to-neutral-400"
         variant="h2">
         Projects I have built
         <br />
         in the past
       </Text>
+      <div className="absolute left-0 right-0 top-0 bottom-0 bg-primary/20 blur-3xl" />
     </Column>
   );
 };
