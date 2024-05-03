eac04a7ca57fa29ff8cad306eac2c1548064cf97
Humboorgir <hardcraftap@gmail.com>
2024-05-03 21:20:16 +0330

    feat: implement about section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,12 +8,19 @@ import ContactSection from "@/components/home/contact-section";
 import Container from "@/components/ui/container";
 import Text from "@/components/ui/text";
 import Column from "@/components/ui/column";
+import Row from "@/components/ui/row";
 import CountUp from "react-countup";
 import { FaNodeJs as NodejsIcon } from "react-icons/fa";
 import { IoLogoDiscord as DiscordIcon } from "react-icons/io5";
-
+import { IoMdInformationCircle as InfoIcon } from "react-icons/io";
 import { FaReact as ReactIcon } from "react-icons/fa";
-import Row from "@/components/ui/row";
+import {
+  SiTailwindcss as TailwindcssIcon,
+  SiFramer as FramerMotionIcon,
+  SiTypescript as TypescriptIcon,
+} from "react-icons/si";
+import { cubicBezier, m } from "framer-motion";
+import Image from "next/image";
 
 const Home = () => {
   return (
@@ -32,6 +39,76 @@ const Home = () => {
       />
       <HeroSection />
 
+      <div className="relative py-20 bg-primary/10 backdrop-blur-[1px]">
+        <div className="absolute -z-10 right-0 top-[50%] translate-y-[-50%] w-[50%] h-full bg-blue-900/20 blur-3xl rounded-full" />
+        <div className="absolute -z-10 left-0 top-[50%] translate-y-[-50%] w-[50%] h-full bg-primary/20 blur-3xl rounded-full" />
+        <Container className="flex items-center w-full">
+          <Column items="start">
+            <span
+              className="mb-6 flex items-center border-2 border-green-500/20 px-3 py-1 rounded-full w-fit
+            bg-green-500/10">
+              <InfoIcon className="h-5 w-5 text-green-500 mr-1.5" />
+              <span className="bg-clip-text text-transparent bg-gradient-to-br from-green-400 to-green-600">
+                About me
+              </span>
+            </span>
+            <Text className="mb-4 text-foreground/90" variant="h2">
+              Hi there! I'm Iliya
+            </Text>
+            <Text className="max-w-prose mb-4" variant="p">
+              Hey there! I'm Iliya, a 15 year old high school student and full stack web developer from
+              Iran. I've been programming since early 2020 or in other words, since I had just turned 12!
+              I've been using computers since the age of 5 and have always shown a huge passion for them.
+              During all these years I've learned a lot and have made countless amounts of projects. In
+              my early days of programming, I started off by back end development however, It's been 1.5
+              years since I've been primarily learning and working on front end development (alongside
+              with back end development) and have managed to learn CSS and most major frontend frameworks
+              to a very high degree and as mentioned earlier, I'm very passionate towards my work and
+              always open to gain new skills.
+            </Text>
+            <Row className="space-x-10">
+              {[
+                { label: "Discord members", amount: 1526 },
+                { label: "Open source projects", amount: 116 },
+                { label: "Satisfied clients", amount: 96 },
+              ].map(({ label, amount }) => {
+                return (
+                  <Column key={amount}>
+                    <Text variant="h2">
+                      <CountUp start={0} end={amount} enableScrollSpy duration={2} suffix="+" />
+                    </Text>
+                    <Text variant="p">{label}</Text>
+                  </Column>
+                );
+              })}
+            </Row>
+          </Column>
+          <m.div
+            whileInView={{
+              gap: "80px",
+              rotate: "12deg",
+            }}
+            transition={{
+              duration: 2,
+              ease: cubicBezier(0.16, 1, 0.3, 1),
+            }}
+            className="gap-4 h-fit w-fit ml-20 grid grid-cols-2 grid-rows-2">
+            <div className="p-5 bg-gradient-to-b from-blue-950 to-transparent w-fit h-fit rounded-full">
+              <ReactIcon className="w-16 h-16 text-blue-500" />
+            </div>
+            <div className="p-5 bg-gradient-to-b from-blue-950 to-transparent w-fit h-fit rounded-full">
+              <TailwindcssIcon className="w-16 h-16 text-blue-600" />
+            </div>
+            <div className="p-5 bg-gradient-to-b from-blue-950 to-transparent w-fit h-fit rounded-full">
+              <TypescriptIcon className="w-16 h-16 text-blue-400" />
+            </div>
+            <div className="p-5 bg-gradient-to-b from-blue-950 to-transparent w-fit h-fit rounded-full">
+              <FramerMotionIcon className="w-16 h-16 text-blue-500" />
+            </div>
+          </m.div>
+        </Container>
+      </div>
+
       <Container className="relative py-20 flex flex-col items-center">
         <div
           className="absolute top-0 left-0 h-[200px] w-[400px]
@@ -47,7 +124,7 @@ const Home = () => {
         <Text className="mb-4 text-foreground/80" variant="h2">
           Fields I'm experienced at
         </Text>
-        <Row className="items-stretch space-x-4">
+        <Row className="items-stretch justify-center flex-wrap">
           {[
             {
               label: "Front-end Development",
@@ -70,7 +147,7 @@ const Home = () => {
           ].map(({ label, description, Icon }) => {
             return (
               <Column
-                className="text-center relative p-6 w-[280px] rounded-md border-2 border-blue-300/10 backdrop-blur-sm"
+                className="mr-4 mb-4 text-center relative p-6 w-[280px] rounded-md border-2 border-blue-300/10 backdrop-blur-sm"
                 key={label}>
                 <div
                   className="absolute top-0 left-[50%] translate-x-[-50%] w-[50%] h-[2px] bg-gradient-to-r 
