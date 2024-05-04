18f809abc43e21ab59a323204787735e1ca084e8
Humboorgir <hardcraftap@gmail.com>
2024-05-04 09:48:36 +0330

    feat(ProjectsSection): rework title

--- src/components/home/projects-section/projects-section.tsx
+++ src/components/home/projects-section/projects-section.tsx
@@ -40,46 +40,46 @@ const ProjectsSection = () => {
     },
   ];
   return (
-    <Container className="flex flex-col items-center mb-36">
-      {/* title  */}
+    <>
       <Title />
+      <Container className="flex flex-col items-center mb-36">
+        {/* projects  */}
+        {projects.map((project, i) => {
+          const isEven = (i + 1) % 2 == 0;
+          return <Project key={project.repo} project={project} isEven={isEven} />;
+        })}
 
-      {/* projects  */}
-      {projects.map((project, i) => {
-        const isEven = (i + 1) % 2 == 0;
-        return <Project project={project} isEven={isEven} />;
-      })}
+        <Row className="items-stretch w-full justify-end px-5 h-fit min-h-[1.5px]" items="start">
+          <Column className="items-end mb-36">
+            <Text className="text-foreground/90" variant="h3">
+              More Projects
+            </Text>
+            <Text className="mb-3 text-right max-w-xl" variant="p">
+              You thought that was it? During my years of programming, I have built a huge number of open
+              source projects which you can view from my Github page
+            </Text>
+            <Row>
+              <Button
+                className="bg-foreground/90 hover:bg-foreground/70 text-black grow w-[220px] rounded-full"
+                variant="ghost"
+                href="https://github.com/humboorgir"
+                openInNewTab>
+                Github page <Github className="mb-[3px] ml-2" />
+              </Button>
+            </Row>
+          </Column>
 
-      <Row className="items-stretch w-full justify-end px-5 h-fit min-h-[1.5px]" items="start">
-        <Column className="items-end mb-36">
-          <Text className="text-foreground/90" variant="h3">
-            More Projects
-          </Text>
-          <Text className="mb-3 text-right max-w-xl" variant="p">
-            You thought that was it? During my years of programming, I have built a huge number of open
-            source projects which you can view from my Github page
-          </Text>
-          <Row>
-            <Button
-              className="bg-foreground/90 hover:bg-foreground/70 text-black grow w-[220px] rounded-full"
-              variant="ghost"
-              href="https://github.com/humboorgir"
-              openInNewTab>
-              Github page <Github className="mb-[3px] ml-2" />
-            </Button>
-          </Row>
-        </Column>
-
-        <Column className="left-0 top-0 ml-3">
-          {/* icon */}
-          <div className="border border-ring rounded-full w-fit p-1.5">
-            <Github className="text-[26.5px] text-white" />
-          </div>
-          {/* line  */}
-          <div className="w-[1.5px] h-full bg-ring" />
-        </Column>
-      </Row>
-    </Container>
+          <Column className="left-0 top-0 ml-3">
+            {/* icon */}
+            <div className="border border-ring rounded-full w-fit p-1.5">
+              <Github className="text-[26.5px] text-white" />
+            </div>
+            {/* line  */}
+            <div className="w-[1.5px] h-full bg-ring" />
+          </Column>
+        </Row>
+      </Container>
+    </>
   );
 };
 
--- src/components/home/projects-section/title.tsx
+++ src/components/home/projects-section/title.tsx
@@ -1,26 +1,51 @@
 import Column from "@/components/ui/column";
+import Container from "@/components/ui/container";
 import Text from "@/components/ui/text";
-import Image from "next/image";
-import { IoSearch } from "react-icons/io5";
-import { PiDetectiveFill } from "react-icons/pi";
+import { PiDetectiveFill as DetectiveIcon } from "react-icons/pi";
 
 const Title = () => {
   return (
-    <Column className="relative mb-16 p-5 px-10">
+    <Container className="relative w-full py-10 mt-16">
+      <div
+        className="absolute top-[8px] left-6 w-[50%] h-[2px] bg-gradient-to-r 
+            from-blue-400 to-transparent blur-[1px]"
+        aria-hidden="true"
+      />
+      <div
+        className="absolute top-[50%] translate-y-[-50%] left-6 w-[50%] h-8 bg-gradient-to-r 
+            from-blue-400 to-transparent blur-3xl"
+        aria-hidden="true"
+      />
+      <span
+        className="mb-6 flex items-center border-2 border-blue-500/10 px-3 py-1 rounded-full w-fit
+            bg-blue-500/10">
+        <div className="h-3.5 w-3.5 rounded-full animate-pulse bg-blue-500 mr-1.5" />
+        <span className="bg-clip-text text-transparent bg-gradient-to-br from-blue-400 to-blue-600">
+          Work samples
+        </span>
+      </span>
       <Text
-        className="flex items-center relative bg-clip-text text-xl text-transparent text-center bg-gradient-to-b from-neutral-100 to-neutral-400"
-        variant="lead">
-        <PiDetectiveFill className="h-7 w-7 text-neutral-300 mr-2" /> Take a look at
-      </Text>
-      <Text
-        className="relative bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-100 to-neutral-400"
+        className="relative w-fit bg-clip-text text-transparent bg-gradient-to-b from-neutral-100 to-neutral-400"
         variant="h2">
-        Projects I have built
-        <br />
-        in the past
+        <div className="mb-1.5">Projects I have built</div>
+        <div>in the past</div>
       </Text>
-      <div className="absolute left-0 right-0 top-0 bottom-0 bg-primary/20 blur-3xl" />
-    </Column>
+    </Container>
+    // <Column className="relative mb-16 p-5 px-10">
+    //   <Text
+    //     className="flex items-center relative bg-clip-text text-xl text-transparent text-center bg-gradient-to-b from-neutral-100 to-neutral-400"
+    //     variant="lead">
+    //     <PiDetectiveFill className="h-7 w-7 text-neutral-300 mr-2" /> Take a look at
+    //   </Text>
+    //   <Text
+    //     className="relative bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-100 to-neutral-400"
+    //     variant="h2">
+    //     Projects I have built
+    //     <br />
+    //     in the past
+    //   </Text>
+    //   <div className="absolute left-0 right-0 top-0 bottom-0 bg-primary/20 blur-3xl" />
+    // </Column>
   );
 };
 
