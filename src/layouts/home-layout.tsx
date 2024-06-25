083d162766158255813fa7424ba9fdc804934d4b
Humboorgir <hardcraftap@gmail.com>
2024-06-25 09:23:58 +0330

    fix: bg color not taking up the entire viewport
    - set background and text color to 'body' element
    - this also fixes lots of potential bugs since body is the element with the lowest z-index

--- src/layouts/home-layout.tsx
+++ src/layouts/home-layout.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const HomeLayout = ({ children }: Props) => {
   return (
-    <div className="grid grid-rows-[auto,1fr,auto] min-h-screen bg-background text-foreground-light">
+    <div className="grid grid-rows-[auto,1fr,auto] min-h-screen">
       <Navbar />
       <div>{children}</div>
       <Footer />
