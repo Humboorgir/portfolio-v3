6d3f593b6fa4c96c1b7cc02aa61ba180bd723ac2
Humboorgir <hardcraftap@gmail.com>
2024-08-26 09:44:25 +0330

    feat(Blog): make thumbnails work with slideShow

--- src/components/blog/slide-show.tsx
+++ src/components/blog/slide-show.tsx
@@ -5,6 +5,7 @@ import Text from "@/components/ui/text";
 import { AnimatePresence, m } from "framer-motion";
 import React, { useCallback, useEffect, useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 type SlideShowProps = {
   posts: Post[];
@@ -31,7 +32,7 @@ const SlideShow = ({ posts }: SlideShowProps) => {
   return (
     <Link
       href={currentPost.url}
-      className="text-transparent max-w-[620px] min-h-[220px] min-w-[200px] sm:min-w-[360px] grow relative flex overflow-hidden"
+      className="text-transparent max-w-[620px] h-[220px] min-w-[200px] sm:min-w-[360px] grow relative flex overflow-hidden"
     >
       .
       <AnimatePresence
@@ -61,36 +62,46 @@ const SlideShow = ({ posts }: SlideShowProps) => {
             scale: 1.5,
             position: "absolute",
           }}
-          className="right-0 top-0 absolute w-full flex flex-col items-end border-neutral-700 border rounded-md p-3
-    bg-gradient-to-b from-white/30 to-90% to-black h-full"
+          className="right-0 top-0 grid grid-cols-[1fr] absolute w-full border-neutral-700 border rounded-md
+    bg-gradient-to-b from-transparent to-80% to-black h-full max-h-[220px]"
           key={currentPost.title}
         >
-          <Text
-            className="mt-auto mr-auto text-foreground/90 font-bold text-xl md:text-xl"
-            variant="h5"
-          >
-            {currentPost.title.split(" ").map((word, index) => (
-              <m.span
-                key={currentPost.title + index}
-                initial={{ opacity: 0, y: 10, filter: "blur(8px)" }}
-                animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
-                transition={{
-                  delay: index * 0.08,
-                  duration: 0.4,
-                }}
-                className="mr-1 inline-block"
-              >
-                {word}
-              </m.span>
-            ))}
-          </Text>
-          <div className="flex mt-1 justify-between items-center w-full">
-            <Text className="md:text-sm" variant="lead">
-              ~{currentPost.readTime} min read
+          <Image
+            className="w-full object-cover h-full max-h-[220px] row-start-1 col-start-1 rounded-md
+            [mask-image:linear-gradient(to_top,transparent_20%,black)]"
+            height={320}
+            width={320}
+            src={currentPost.thumbnail}
+            alt={currentPost.title}
+          />
+          <div className="flex flex-col items-end justify-end row-start-1 col-start-1 p-3 rounded-md">
+            <Text
+              className="mr-auto text-foreground/90 font-bold text-xl md:text-xl"
+              variant="h5"
+            >
+              {currentPost.title.split(" ").map((word, index) => (
+                <m.span
+                  key={currentPost.title + index}
+                  initial={{ opacity: 0, y: 10, filter: "blur(8px)" }}
+                  animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
+                  transition={{
+                    delay: index * 0.08,
+                    duration: 0.4,
+                  }}
+                  className="mr-1 inline-block"
+                >
+                  {word}
+                </m.span>
+              ))}
             </Text>
-            <Text className="md:text-sm" variant="lead">
-              {currentPost.postedOn}
-            </Text>
+            <div className="flex mt-1 justify-between items-center w-full">
+              <Text className="md:text-sm" variant="lead">
+                ~{currentPost.readTime} min read
+              </Text>
+              <Text className="md:text-sm" variant="lead">
+                {currentPost.postedOn}
+              </Text>
+            </div>
           </div>
         </m.div>
       </AnimatePresence>
