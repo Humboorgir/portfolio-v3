2dc0ddc6f73d3d3627e6792c896274072a32e70a
Humboorgir <hardcraftap@gmail.com>
2024-12-24 18:01:05 +0330

    feat(home): add metadata

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,8 @@ import FAQSection from "@/components/home/faq-section";
 import ContactSection from "@/components/home/contact-section";
 import BlogSection from "@/components/home/blog-section";
 
+import Head from "next/head";
+
 const Home = () => {
   return (
     // TODO: fix the bug described below
@@ -16,44 +18,56 @@ const Home = () => {
     // not look the same on chrome and firefox
     // to fix this, reduce the blur amount of the elements to 100px and reduce their opacity
     // this might not produce the exact same result but it will be close.
-    <div className="relative antialiased">
-      {/* background grid  */}
-      <svg
-        className="absolute top-0 left-0 bottom-0 right-0 opacity-20"
-        width="100%"
-        height="100%"
-      >
-        <pattern
-          id="smallGrid"
-          width="25"
-          height="25"
-          patternUnits="userSpaceOnUse"
-        >
-          <circle cx="10" cy="10" r="1" fill="#FFFFFF" />
-        </pattern>
-        <rect width="100%" height="100%" fill="url(#smallGrid)" />
-      </svg>
+    <>
+      {/* metadata  */}
+      <Head>
+        <title>IliyaFaz ~ Portfolio</title>
 
-      <div
-        className="absolute top-0 left-[50%] translate-x-[-50%] h-[200px] w-[600px] max-w-[60vw]
+        <meta
+          name="description"
+          content="I'm Iliya, a self-taught, full-stack web developer with a deep passion for programming. 
+          This is my portfolio, where you can find out more about me, take a look at projects I've previously built
+          or even reach out to me!"
+        />
+      </Head>
+      {/* page content  */}
+      <div className="relative antialiased">
+        {/* background grid  */}
+        <svg
+          className="absolute top-0 left-0 bottom-0 right-0 opacity-20"
+          width="100%"
+          height="100%">
+          <pattern
+            id="smallGrid"
+            width="25"
+            height="25"
+            patternUnits="userSpaceOnUse">
+            <circle cx="10" cy="10" r="1" fill="#FFFFFF" />
+          </pattern>
+          <rect width="100%" height="100%" fill="url(#smallGrid)" />
+        </svg>
+
+        <div
+          className="absolute top-0 left-[50%] translate-x-[-50%] h-[200px] w-[600px] max-w-[60vw]
        bg-gradient-to-r from-primary/80 to-accent/60 blur-[140px]"
-      />
-      <HeroSection />
+        />
+        <HeroSection />
 
-      <AboutSection />
+        <AboutSection />
 
-      <ExperienceSection />
+        <ExperienceSection />
 
-      <ProjectsSection />
+        <ProjectsSection />
 
-      <PricingSection />
+        <PricingSection />
 
-      <FAQSection />
+        <FAQSection />
 
-      <ContactSection />
+        <ContactSection />
 
-      <BlogSection />
-    </div>
+        <BlogSection />
+      </div>
+    </>
   );
 };
 
